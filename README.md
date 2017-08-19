3f063cd9aaff0614948daee99fafc4641b69d4e6
Brian Scaturro <scaturrob@gmail.com>
2017-08-19 11:21:51 -0400

    Updates license info and README

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # yoose
 
-A Clojure library designed to ... well, that part is up to you.
+<p align="center">
+  <img src="https://raw.github.com/brianium/yoose/master/yussss.gif" alt="Finn Says Yussssss" />
+</p>
 
 ## Usage
 
-FIXME
 
-## License
-
-Copyright © 2017 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject yoose "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A small library for use case driven development"
   
-  :url "http://example.com/FIXME"
+  :url "https://github.com/brianium/yoose"
   
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"}
   
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/core.async "0.3.443"]]
